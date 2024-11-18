c75da32b9aec580365e262bc921e50d02d8778f8
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-11-18 12:09:45 +0100

    move SIFT_bovw to funtions to a file

---
+++ src/SIFT_bovw.py
@@ -0,0 +1,129 @@
+from scipy.cluster._vq import vq
+from numpy.linalg import norm
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+
+def read_bw_images(img_paths):
+    return [
+        cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2GRAY)
+        if len(cv2.imread(img_path).shape) == 3
+        else cv2.imread(img_path)
+        for img_path in img_paths
+    ]
+
+
+def clean_descriptors(
+    keypoints: list[cv2.KeyPoint], descriptors: list[np.ndarray]
+) -> tuple[list[cv2.KeyPoint], list[np.ndarray]]:
+    print(f"len before: {len(descriptors)}")
+    # initialize list to store idx values of records to drop
+    to_drop = []
+    for i, img_descriptors in enumerate(descriptors):
+        # if there are no descriptors, add record idx to drop list
+        if img_descriptors is None:
+            to_drop.append(i)
+
+    print(f"indexes: {to_drop}")
+    # delete from list in reverse order
+    for i in sorted(to_drop, reverse=True):
+        del descriptors[i], keypoints[i]
+
+    print(f"len after: {len(descriptors)}")
+    return keypoints, descriptors
+
+
+def run_sift(images, clean=True) -> tuple[list[cv2.KeyPoint], list[np.ndarray]]:
+    """
+    Run SIFT on a list of black and white images
+    :param images:
+    :return: list of keypoints and descriptors
+    """
+    sift = cv2.SIFT_create()
+    keypoints_agg: list[cv2.KeyPoint] = []
+    descriptors_agg: list[np.ndarray] = []
+    for img in images:
+        # extract keypoints and descriptors for each image
+        img_keypoints, img_descriptors = sift.detectAndCompute(img, None)
+        keypoints_agg.append(img_keypoints)
+        descriptors_agg.append(img_descriptors)
+
+    return clean_descriptors(
+        keypoints_agg, descriptors_agg
+    ) if clean else keypoints_agg, descriptors_agg
+
+
+def get_visual_words(descriptors, codebook):
+    visual_words = []
+    for img_descriptors in descriptors:
+        # for each image, map each descriptor to the nearest codebook entry
+        img_visual_words, distance = vq(img_descriptors, codebook)
+        visual_words.append(img_visual_words)
+    return visual_words
+
+
+def get_frequency_vectors(visual_words, k):
+    frequency_vectors = []
+    for img_visual_words in visual_words:
+        # create a frequency vector for each image
+        img_frequency_vector = np.zeros(k)
+        for word in img_visual_words:
+            img_frequency_vector[word] += 1
+        frequency_vectors.append(img_frequency_vector)
+    return np.stack(frequency_vectors)
+
+
+def get_tfidf(frequency_vectors):
+    """
+    Compute the TF-IDF for the frequency vectors
+    :param frequency_vectors:
+    :return:
+    """
+    N = len(frequency_vectors)
+    df = np.sum(frequency_vectors > 0, axis=0)
+    print(df.shape, df[:5])
+    idf = np.log(N / df)
+    print(idf.shape, idf[:5])
+    return frequency_vectors * idf
+
+
+def search_test(
+    image: np.ndarray,
+    db: np.ndarray,
+    top_k: int = 5,
+    search_image=None,
+    db_images: list = None,
+) -> np.ndarray:
+    """
+    Search for the most similar images in the database
+    :param image: tfidf vector for the search image
+    :param db: tfidf vectors for the database images
+    :param top_k: number of similar images to return
+    :param search_image: (optional) image to search for
+    :param db_images: (optional) list of images in the database
+    :return: indices of k the most similar images
+    """
+    if search_image:
+        # show the search image
+        print("Search image:")
+        plt.imshow(search_image, cmap="gray")
+        plt.show()
+        print("-----------------------------------------------------")
+    # get search image vector
+    a = image
+    b = db
+
+    # get the cosine distance for the search image `a`
+    cosine_similarity = np.dot(a, b.T) / (norm(a) * norm(b, axis=1))
+    # get the top k indices for most similar vecs
+    idx = np.argsort(-cosine_similarity)[:top_k]
+
+    # display the results
+    if db_images:
+        for i in idx:
+            print(f"{i}: {round(cosine_similarity[i], 4)}")
+            plt.imshow(db_images[i], cmap="gray")
+            plt.show()
+
+    return idx
