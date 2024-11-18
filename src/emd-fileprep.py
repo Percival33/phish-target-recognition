51a87d8ab2d21e717d9f53029ddd4f800c07aa35
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-11-18 11:43:10 +0100

    run format

--- src/emd-fileprep.py
+++ src/emd-fileprep.py
@@ -3,13 +3,13 @@ from os import listdir
 
 from src.config import DATA_DIR
 
-TRAIN_DIR = DATA_DIR / 'phishIRIS_DL_Dataset' / 'train'
-OUTPUT_DIR = DATA_DIR / 'EMD' / 'test_sites'
+TRAIN_DIR = DATA_DIR / "phishIRIS_DL_Dataset" / "train"
+OUTPUT_DIR = DATA_DIR / "EMD" / "test_sites"
 
 
 def main():
     for company in listdir(TRAIN_DIR):
-        if company in ['.DS_Store', 'other']:
+        if company in [".DS_Store", "other"]:
             continue
 
         imgs = listdir(str(TRAIN_DIR / company))
@@ -18,10 +18,10 @@ def main():
             folder_name = f"{company}+{idx}"
             os.mkdir(str(OUTPUT_DIR / folder_name))
             print(folder_name)
-            with open(str(TRAIN_DIR / company / img), 'rb') as src_file:
-                with open(str(OUTPUT_DIR / folder_name / 'shot.png'), 'wb') as dst_file:
+            with open(str(TRAIN_DIR / company / img), "rb") as src_file:
+                with open(str(OUTPUT_DIR / folder_name / "shot.png"), "wb") as dst_file:
                     dst_file.write(src_file.read())
-            with open(str(OUTPUT_DIR / folder_name / 'info.txt'), 'w') as f:
+            with open(str(OUTPUT_DIR / folder_name / "info.txt"), "w") as f:
                 f.write(f"https://www.{company}.com")
 
 
