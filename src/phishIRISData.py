c9a0c583b3a1a36c15c29c48bc26c3c95bcf1df0
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-11-19 14:25:53 +0100

    clustering alg & bag of words (#2)
    add bovw and clustering algs

---
+++ src/phishIRISData.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass, field
+from perception import hashers
+from collections import defaultdict
+from src.config import config
+
+hasher = hashers.PHashF()
+TRAIN_DIR = config.RAW_DATA_DIR / "phishIRIS_DL_Dataset" / "train"
+VAL_DIR = config.RAW_DATA_DIR / "phishIRIS_DL_Dataset" / "val"
+
+
+@dataclass
+class PhishIRISData:
+    img_to_hash: dict = field(default_factory=dict)
+    hash_to_img: dict = field(default_factory=dict)
+    img_per_company: defaultdict = field(default_factory=lambda: defaultdict(list))
+    hash_to_company: defaultdict = field(default_factory=lambda: defaultdict(str))
+    imgs: list = field(default_factory=list)
+    labels: list = field(default_factory=list)
+    _loaded: bool = field(default=False, init=False)  # Track if data has been loaded
+    val_imgs: list = field(default_factory=list)
+    val_labels: list = field(default_factory=list)
+
+    def load_data(self):
+        """Load data from the dataset directory into the data structures."""
+        if self._loaded:
+            return  # Skip loading if already loaded
+
+        for company_path in TRAIN_DIR.iterdir():
+            if not company_path.is_dir() or company_path.name in {".DS_Store", "other"}:
+                continue
+
+            company_name = company_path.name
+            for example_path in company_path.iterdir():
+                if not example_path.is_file():
+                    continue
+
+                self.imgs.append(str(example_path))
+                self.labels.append(str(company_name))
+
+                hsh = hasher.compute(str(example_path))
+                formatted_name = "-".join(example_path.name.split(" "))
+                self.img_to_hash[formatted_name] = hasher.string_to_vector(hsh)
+                self.hash_to_img[hsh] = formatted_name
+                self.hash_to_company[hsh] = company_name
+                self.img_per_company[company_name].append(hsh)
+
+        for company_path in VAL_DIR.iterdir():
+            if not company_path.is_dir() or company_path.name in {".DS_Store", "other"}:
+                continue
+
+            company_name = company_path.name
+            for example_path in company_path.iterdir():
+                if not example_path.is_file():
+                    continue
+
+                self.val_imgs.append(str(example_path))
+                self.val_labels.append(str(company_name))
+
+                hsh = hasher.compute(str(example_path))
+                formatted_name = "-".join(example_path.name.split(" "))
+                self.img_to_hash[formatted_name] = hasher.string_to_vector(hsh)
+                self.hash_to_img[hsh] = formatted_name
+                self.hash_to_company[hsh] = company_name
+                self.img_per_company[company_name].append(hsh)
+
+        self._loaded = True
+
+    def get_data(self):
+        """Return loaded data, ensuring it is loaded first."""
+        if not self._loaded:
+            self.load_data()
+        return self
