e26d9edaf317d957a5aef7ff2be3db0d4bbb58dd
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-01-31 13:14:52 +0100

    fixes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ curl -LsSf https://astral.sh/uv/0.5.18/install.sh | sh
 source $HOME/.local/bin/env
 
 # Download processed dataset
-uv run --with gdown gdown 1ewejN6qo3Bkb8IYSKeklU4GIlRHqPlUC -O - --quiet | tar zxvf - -C 
+uv run --with gdown gdown 1ewejN6qo3Bkb8IYSKeklU4GIlRHqPlUC -O - --quiet | tar zxvf - -C ?FOLDER_PATH?
 
 # Run training
 uv run trainer_phase2.py
