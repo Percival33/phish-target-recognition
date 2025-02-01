4394fc17428fc6881695736d2f1af93c91522b5f
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-02-01 13:11:19 +0100

    update script

--- scripts/vp-setup.sh
+++ scripts/vp-setup.sh
@@ -13,8 +13,17 @@ cd phish-target-recognition && git checkout VP-script1
 # Download processed dataset
 uv run --with gdown gdown 1ewejN6qo3Bkb8IYSKeklU4GIlRHqPlUC -O - --quiet | tar zxvf - -C ../../../data/interim
 
+# smallerSampleDataset
+#uv run --with gdown gdown 1agHs15tIlKvXqC8M65ZXln2mEDPz6fSk -O - --quiet | tar zxvf - -C ../../../data/interim
+
+# vp original paper results on smaller dataset
+uv run --with gdown gdown 1xPN_3GYo8tcdnCPwPdKXzUS13-K54NLg -O - --quiet | tar zxvf - -C ../../../data/interim
+
 # Login to wandb
 uv run wandb login
 
 # Run training
-uv run trainer_phase2.py
+#uv run trainer_phase2.py
+
+# smaller sample dataset
+#uv run trainer.py --num-targets 5 --legit-imgs-num 420 --phish-imgs-num 160 --save-interval 20 --n-iter 20 --num-sets 5 --hard-n-iter 5 --num-sets 5 --lr-interval 250
