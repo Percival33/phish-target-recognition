b716eeff653cef84fe88ae4c336a72cfdb592bb3
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-03-02 11:06:01 +0100

    Vp script1 (#10)
    * backup
    
    * add: VisualPhish - phase 1 run
    
    * WIP
    
    * evaluate func
    
    * add vp script
    
    * create evaluate.py
    
    * fix running evaluate.py
    
    * logs
    
    * phase2 VP
    
    * fix tools
    
    * add tools package
    
    * add tools package
    
    * add tools lib
    
    * fix path in tool project
    
    * update lockfile
    
    * fix path and build
    
    * improve log and fix path
    
    * improve log and fix path
    
    * fix suffix path
    
    * save and load preprocessed data
    
    * save and load preprocessed data - fix
    
    * add debug notebook
    
    * debug
    
    * debug phase2
    
    * fix trainer_phase2.py
    
    * add wandb logging
    
    * fixes
    
    * update lr-interval
    
    * update logging lr
    
    * changed parameters
    
    * add results from training
    
    * add log from interrupted run
    
    * rename inner class
    
    * clean up
    
    * add setup script
    
    * move script 1 to trainer
    
    * add creating test/train split
    
    * fix imports
    
    * rename data to DataHelper
    
    * split runs and iteration count
    
    * fix
    
    * fix targetHelper dependency
    
    * additional logs
    
    * additional logs
    
    * fix summary
    
    * log model and emb
    
    * update tools and logging to use logger.conf
    
    * comment to add handler
    
    * fix import
    
    * update lockfile
    
    * log model as debug
    
    * fix print_fn in model.summary()
    
    * fix log level
    
    * helper
    
    * add attributes
    
    * improve DataHelper
    
    * remove tools package
    
    * Revert "remove tools package"
    
    This reverts commit 14987f4ce7b2ff1347cc6744857a3cff1d09b08d.
    
    * fix scritps to run full dataset
    
    * change input param
    
    * add debug logs
    
    * add debug logs
    
    * change targets name
    
    * create setup_loggin function
    
    * pin uv version
    
    * update uv.lock
    
    * fixes
    
    * update vp-setup.sh
    
    * fix reading file names
    
    * run formatter and check
    
    * change print to logger
    
    * modify ruff setup
    
    * formatter
    
    * add git ignore
    
    * fix ambiguous value
    
    * fix not existing attribute reference
    
    * change batch size
    
    * simplify method
    
    * fix saving indicies issue
    
    * fixes
    
    * fix assert
    
    * simplify method
    
    * create folder
    
    * add tqdm
    
    * add tqdm
    
    * comment out tqdm
    
    * prepare for testing
    
    * update script
    
    * smaller test
    
    * Revert "smaller test"
    
    This reverts commit ef58138d5dc491b3d56a7badd0ed99402978b262.
    
    * fixes
    
    * update eval script
    
    * fix eval.py script
    
    * simplify eval script
    
    * fix evaluation
    
    * log model
    
    * add logging oom
    
    * remove model.compile(options)
    
    * remove model.compile(options)
    
    * add logging
    
    * change script
    
    * change TargetHelper.py
    
    * add fixes
    
    * log memory usage
    
    * log memory usage
    
    * add file handler
    
    * simplify logging
    
    * add tensorboard profile
    
    * add tensorboard logging
    
    * fix dump debug
    
    * fixes for logging
    
    * upate tensorboard versions
    
    * use numpy vectorization
    
    * simplify code
    
    * comment out profiling
    
    * comment out profiling
    
    * update dependencies
    
    * fixes
    
    * use tf.tensor in trainer
    
    * Revert "use tf.tensor in trainer"
    
    This reverts commit f971471d9819c7fe640593087a275628d2f77a30.
    
    * fixes while phase 2 training
    
    * local variable 'X_train_new' referenced l251
    
    * fix
    
    * change training params (iters to 75)
    
    * fix format
    
    * add eval script
    
    * eval script with ROC
    
    * fix eval-new
    
    * eval script with ROC plotting
    
    * eval script with ROC plotting
    
    * cleanup

---
+++ justfile
@@ -0,0 +1,5 @@
+prepare-visualphish:
+    cd src/models/visualphish && \
+    uv sync --frozen && \
+    uv run --with gdown gdown 1ewejN6qo3Bkb8IYSKeklU4GIlRHqPlUC -O - --quiet | tar zxvf - -C ../../data/raw/VisualPhish && \
+
---
+++ src/models/visualphishnet/ModelHelper.py
@@ -0,0 +1,244 @@
+import logging
+import logging.config
+
+import numpy as np
+from keras import Input, Model, optimizers
+from keras import backend as K
+from keras.applications import VGG16
+from keras.layers import Conv2D, GlobalMaxPooling2D, Lambda, Reshape, Subtract
+from keras.models import load_model
+from keras.regularizers import l2
+from tools.config import setup_logging
+
+import DataHelper as data
+from Evaluate import Evaluate
+from TargetHelper import TargetHelper
+
+
+class ModelHelper:
+    def __init__(self):
+        setup_logging()
+        self.logger = logging.getLogger(__name__)
+
+    def prepare_model(self, input_shape, new_conv_params, margin, lr):
+        model = self.define_triplet_network(input_shape, new_conv_params)
+        model.summary(print_fn=self.logger.debug)
+
+        optimizer = optimizers.Adam(lr=lr)
+        model.compile(loss=self.custom_loss(margin), optimizer=optimizer)
+        self.logger.debug("Model compiled")
+        return model
+
+    def load_trained_model(self, output_dir, saved_model_name, margin, lr):
+        model = load_model(
+            output_dir / f"{saved_model_name}.h5",
+            custom_objects={"loss": self.custom_loss(margin)},
+        )
+
+        optimizer = optimizers.Adam(lr=lr)
+        model.compile(loss=self.custom_loss(margin), optimizer=optimizer)
+
+        return model
+
+    def load_model(self, output_dir, saved_model_name, margin):
+        return load_model(
+            output_dir / f"{saved_model_name}.h5",
+            custom_objects={"loss": self.custom_loss(margin)},
+        )
+
+    def get_embeddings(
+        self,
+        full_model,
+        X_train_legit,
+        y_train_legit,
+        all_imgs_test,
+        all_labels_test,
+        test_idx,
+        train_idx,
+    ) -> data.TrainResults:
+        self.logger.info("Starting to calculate embeddings...")
+
+        shared_model = full_model.layers[3]  # partial model to get embeddings
+        full_model.summary(print_fn=self.logger.debug)
+        whitelist_emb = shared_model.predict(X_train_legit, batch_size=64)
+        phishing_emb = shared_model.predict(all_imgs_test, batch_size=64)
+
+        self.logger.info("Embeddings were calculated")
+
+        return data.TrainResults(
+            X_legit_train=whitelist_emb,
+            y_legit_train=y_train_legit,
+            X_phish=phishing_emb,
+            y_phish=all_labels_test,
+            phish_test_idx=test_idx,
+            phish_train_idx=train_idx,
+        )
+
+    def get_acc(
+        self,
+        targetHelper: TargetHelper,
+        VPTrainResults: data.TrainResults,
+        trusted_list_path,
+        phishing_path,
+        all_file_names_train=None,
+        all_file_names_test=None,
+    ):
+        # TODO: enable using wandb artifacts
+        self.logger.info("Preparing to calculate acc...")
+        if all_file_names_train is not None:
+            self.logger.info("Using all_file_names_train")
+        if all_file_names_test is not None:
+            self.logger.info("Using all_file_names_test")
+
+        legit_file_names = (
+            all_file_names_train
+            if all_file_names_train is not None
+            else targetHelper.read_file_names(trusted_list_path, "targets.txt")
+        )
+        phish_file_names = (
+            all_file_names_test
+            if all_file_names_test is not None
+            else targetHelper.read_file_names(phishing_path, "targets.txt")
+        )
+        phish_train_files, phish_test_files = data.get_phish_file_names(
+            phish_file_names,
+            VPTrainResults.phish_train_idx,
+            VPTrainResults.phish_test_idx,
+        )
+
+        evaluate = Evaluate(VPTrainResults, legit_file_names, phish_train_files)
+
+        """
+        correct_matches = sum(
+            targetHelper.check_if_target_in_top(
+                str(test_file.name), 
+                evaluate.find_names_min_distances(
+                    *evaluate.find_min_distances(
+                        np.ravel(evaluate.pairwise_distance[i, :]), 
+                        1
+                    )
+                )[1]
+            )[0] 
+            for i, test_file in enumerate(phish_test_files)
+        )
+       """
+
+        n = 1  # Top-1 match
+        correct = 0
+        self.logger.info(f"Calculating acc with top-{n} match")
+        for i, test_file in enumerate(phish_test_files):
+            distances_to_train = evaluate.pairwise_distance[i, :]
+            names_min_distance, only_names, min_distances = evaluate.find_names_min_distances(
+                *evaluate.find_min_distances(np.ravel(distances_to_train), n)
+            )
+            found, found_idx = targetHelper.check_if_target_in_top(str(test_file), only_names)
+            self.logger.debug(names_min_distance)
+
+            if found == 1:
+                correct += 1
+
+        accuracy = correct / len(phish_test_files)
+        self.logger.info(f"Accuracy: {accuracy:.2%}")
+        return accuracy
+
+    def save_model(self, model, output_dir, model_name):
+        # TODO: save artifact to wandb
+        model.save(output_dir / f"{model_name}.h5")
+        self.logger.info("Saved model to disk")
+
+    @staticmethod
+    def define_triplet_network(input_shape, new_conv_params):
+        """
+        Input_shape: shape of input images
+        new_conv_params: dimension of the new convolution layer [spatial1,spatial2,channels]
+        """
+
+        # Define the tensors for the three input images
+        anchor_input = Input(input_shape)
+        positive_input = Input(input_shape)
+        negative_input = Input(input_shape)
+
+        # Use VGG as a base model
+        base_model = VGG16(weights="imagenet", input_shape=input_shape, include_top=False)
+
+        x = base_model.output
+        x = Conv2D(
+            new_conv_params[2],
+            (new_conv_params[0], new_conv_params[1]),
+            activation="relu",
+            kernel_initializer="he_normal",
+            kernel_regularizer=l2(2e-4),
+        )(x)
+        x = GlobalMaxPooling2D()(x)
+        model = Model(inputs=base_model.input, outputs=x)
+
+        # Generate the encodings (feature vectors) for the two images
+        encoded_a = model(anchor_input)
+        encoded_p = model(positive_input)
+        encoded_n = model(negative_input)
+
+        mean_layer = Lambda(lambda x: K.mean(x, axis=1))
+
+        square_diff_layer = Lambda(lambda tensors: K.square(tensors[0] - tensors[1]))
+        square_diff_pos = square_diff_layer([encoded_a, encoded_p])
+        square_diff_neg = square_diff_layer([encoded_a, encoded_n])
+
+        square_diff_pos_l2 = mean_layer(square_diff_pos)
+        square_diff_neg_l2 = mean_layer(square_diff_neg)
+
+        # Add a diff layer
+        diff = Subtract()([square_diff_pos_l2, square_diff_neg_l2])
+        diff = Reshape((1,))(diff)
+
+        # Connect the inputs with the outputs
+        triplet_net = Model(inputs=[anchor_input, positive_input, negative_input], outputs=diff)
+
+        # return the model
+        return triplet_net
+
+    @staticmethod
+    def custom_loss(margin):
+        def loss(y_true, y_pred):
+            loss_value = K.maximum(y_true, margin + y_pred)
+            loss_value = K.mean(loss_value, axis=0)
+            return loss_value
+
+        return loss
+
+    @staticmethod
+    def get_model_memory_usage(batch_size, model):
+        import numpy as np
+
+        try:
+            from keras import backend as K
+        except ImportError:
+            from tensorflow.keras import backend as K
+
+        shapes_mem_count = 0
+        internal_model_mem_count = 0
+        for layer in model.layers:
+            layer_type = layer.__class__.__name__
+            if layer_type == "Model":
+                internal_model_mem_count += ModelHelper.get_model_memory_usage(batch_size, layer)
+            single_layer_mem = 1
+            out_shape = layer.output_shape
+            if type(out_shape) is list:
+                out_shape = out_shape[0]
+            for s in out_shape:
+                if s is None:
+                    continue
+                single_layer_mem *= s
+            shapes_mem_count += single_layer_mem
+
+        trainable_count = np.sum([K.count_params(p) for p in model.trainable_weights])
+        non_trainable_count = np.sum([K.count_params(p) for p in model.non_trainable_weights])
+
+        number_size = 4.0
+        if K.floatx() == "float16":
+            number_size = 2.0
+        if K.floatx() == "float64":
+            number_size = 8.0
+
+        total_memory = number_size * (batch_size * shapes_mem_count + trainable_count + non_trainable_count)
+        gbytes = np.round(total_memory / (1024.0**3), 3) + internal_model_mem_count
+        return gbytes
