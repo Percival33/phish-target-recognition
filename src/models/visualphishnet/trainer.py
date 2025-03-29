0a809e193c9f51aae8fdc1d31106c1ccd59889a4
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-03-29 12:18:42 +0100

    fix memory leak - test 1

--- src/models/visualphishnet/trainer.py
+++ src/models/visualphishnet/trainer.py
@@ -469,22 +469,22 @@ def reset_keras(model):
 
 
 if __name__ == "__main__":
-    gpus = tf.config.list_physical_devices("GPU")
-    if gpus:
-        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
-        try:
-            tf.config.set_logical_device_configuration(
-                gpus[0],
-                [
-                    tf.config.LogicalDeviceConfiguration(memory_limit=8192),
-                    tf.config.LogicalDeviceConfiguration(memory_limit=8192),
-                ],
-            )
-            logical_gpus = tf.config.list_logical_devices("GPU")
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Virtual devices must be set before GPUs have been initialized
-            print(e)
+    # gpus = tf.config.list_physical_devices("GPU")
+    # if gpus:
+    #     # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    #     try:
+    #         tf.config.set_logical_device_configuration(
+    #             gpus[0],
+    #             [
+    #                 tf.config.LogicalDeviceConfiguration(memory_limit=8192),
+    #                 tf.config.LogicalDeviceConfiguration(memory_limit=8192),
+    #             ],
+    #         )
+    #         logical_gpus = tf.config.list_logical_devices("GPU")
+    #         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    #     except RuntimeError as e:
+    #         # Virtual devices must be set before GPUs have been initialized
+    #         print(e)
 
     # gpus = tf.config.experimental.list_physical_devices("GPU")
     # for gpu in gpus:
