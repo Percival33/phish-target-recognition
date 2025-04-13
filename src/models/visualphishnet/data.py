2a54a68a813c327dbab1aba634dbaf1495dee987
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-04-13 22:50:46 +0200

    Phishpedia reprod (#15)
    * Add Phishpedia and VisualPhish to API (#14)
    
    * Fixup API
    
    * add phishpedia
    
    * fix PhishpediaServing.py
    
    * fix dependencies
    
    * reorganize folders
    
    * not working
    
    * justfile fix
    
    * fix docker build
    
    * update dockerignore
    
    * update API
    
    * update API
    
    * fixes
    
    * added calculated files
    
    * update API - vp
    
    * update API - vp
    
    * fixes VP api
    
    * fix path problem
    
    * encode image
    
    * encode image fix
    
    * encode image fix2
    
    * encode image - send base64
    
    * encode image - send base64
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    * encode image - debug
    
    # Conflicts:
    #	.gitignore
    #	src/models/visualphishnet/Evaluate.py
    
    * fixup

---
+++ src/models/visualphishnet/data.py
@@ -0,0 +1,42 @@
+# TODO: rename file to make it more descriptive
+import os
+from skimage.transform import resize
+from matplotlib.pyplot import imread
+
+import numpy as np
+
+
+def read_imgs_per_website(data_path, targets, imgs_num, reshape_size, start_target_count):
+    all_imgs = np.zeros(shape=[imgs_num, 224, 224, 3])
+    all_labels = np.zeros(shape=[imgs_num, 1])
+
+    all_file_names = []
+    targets_list = targets.splitlines()
+    count = 0
+    for i in range(0, len(targets_list)):
+        target_path = data_path / targets_list[i]
+        print(target_path)
+        file_names = sorted(os.listdir(target_path))
+        for j in range(0, len(file_names)):
+            try:
+                img = imread(target_path / file_names[j])
+                img = img[:, :, 0:3]
+                all_imgs[count, :, :, :] = resize(img, (reshape_size[0], reshape_size[1]), anti_aliasing=True)
+                all_labels[count, :] = i + start_target_count
+                all_file_names.append(file_names[j])
+                count = count + 1
+            except:
+                #some images were saved with a wrong extensions
+                try:
+                    img = imread(target_path / file_names[j], format='jpeg')
+                    img = img[:, :, 0:3]
+                    all_imgs[count, :, :, :] = resize(img, (reshape_size[0], reshape_size[1]), anti_aliasing=True)
+                    all_labels[count, :] = i + start_target_count
+                    all_file_names.append(file_names[j])
+                    count = count + 1
+                except:
+                    print('failed at:')
+                    print('***')
+                    print(file_names[j])
+                    break
+    return all_imgs, all_labels, all_file_names
