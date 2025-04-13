70bfdf6a5f4313887ca079b97783ff470ea2d61d
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-04-13 23:17:42 +0200

    add new metrics

--- src/tools/src/tools/metrics.py
+++ src/tools/src/tools/metrics.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import f1_score, roc_auc_score, matthews_corrcoef
+from sklearn.metrics import f1_score, roc_auc_score, matthews_corrcoef, precision_score, recall_score
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -6,7 +6,9 @@ def calculate_metrics(cls_true, cls_pred, targets_true, targets_pred):
     class_metrics = {
         "f1_weighted": f1_score(cls_true, cls_pred, average='weighted'),
         "roc_auc": roc_auc_score(cls_true, cls_pred),
-        "mcc": matthews_corrcoef(cls_true, cls_pred)
+        "mcc": matthews_corrcoef(cls_true, cls_pred),
+        "precision": precision_score(cls_true, cls_pred, average='macro'),
+        "recall": recall_score(cls_true, cls_pred, average='macro'),
     }
 
     all_targets = list(set(targets_true + targets_pred))
@@ -20,7 +22,9 @@ def calculate_metrics(cls_true, cls_pred, targets_true, targets_pred):
         "target_f1_micro": f1_score(targets_true_encoded, targets_pred_encoded, average='micro'),
         "target_f1_macro": f1_score(targets_true_encoded, targets_pred_encoded, average='macro'),
         "target_f1_weighted": f1_score(targets_true_encoded, targets_pred_encoded, average='weighted'),
-        "target_mcc": matthews_corrcoef(targets_true_encoded, targets_pred_encoded)
+        "target_mcc": matthews_corrcoef(targets_true_encoded, targets_pred_encoded),
+        "precision": precision_score(cls_true, cls_pred, average='macro'),
+        "recall": recall_score(cls_true, cls_pred, average='macro'),
     }
 
     return class_metrics, target_metrics
