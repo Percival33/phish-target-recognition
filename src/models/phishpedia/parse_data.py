294516cf95f656564d44c93a1b89161c25ddd513
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-04-22 04:21:57 +0200

    run lint

--- src/models/phishpedia/parse_data.py
+++ src/models/phishpedia/parse_data.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import shutil
 from tools.config import RAW_DATA_DIR, INTERIM_DATA_DIR
 from tqdm import tqdm
-
+import argparse
 
 def copy_and_rename_images(src_folder, dest_folder, is_phish):
     src_folder = Path(src_folder)
@@ -41,8 +41,25 @@ def copy_and_rename_images(src_folder, dest_folder, is_phish):
 
 
 if __name__ == "__main__":
-    # src_folder = RAW_DATA_DIR / 'phishpedia' / 'benign_sample_30k'
-    src_folder = RAW_DATA_DIR / "phishpedia" / "phish_sample_30k"
-    # dest_folder = INTERIM_DATA_DIR / 'phishpedia' / 'benign_sample_30k'
-    dest_folder = INTERIM_DATA_DIR / "phishpedia" / "phish_sample_30k"
-    copy_and_rename_images(src_folder, dest_folder, is_phish=True)
+    parser = argparse.ArgumentParser(description="Copy and rename images from source to destination folder")
+    parser.add_argument(
+        "--src_folder",
+        type=str,
+        default=str(RAW_DATA_DIR / "phishpedia" / "phish_sample_30k"),
+        help="Source folder containing images"
+    )
+    parser.add_argument(
+        "--dest_folder", 
+        type=str,
+        default=str(INTERIM_DATA_DIR / "phishpedia" / "phish_sample_30k"),
+        help="Destination folder for renamed images"
+    )
+    parser.add_argument(
+        "--is_phish",
+        type=bool,
+        default=True,
+        help="Whether the images are phishing samples"
+    )
+
+    args = parser.parse_args()
+    copy_and_rename_images(args.src_folder, args.dest_folder, args.is_phish)
