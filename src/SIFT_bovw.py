e8c70ebfe5a2ae64d0034e4175c3329637f03e8f
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-09-11 23:47:29 +0200

    clean up

--- src/SIFT_bovw.py
+++ src/SIFT_bovw.py
@@ -18,15 +18,12 @@ def clean_descriptors(
     keypoints: list[cv2.KeyPoint], descriptors: list[np.ndarray]
 ) -> tuple[list[cv2.KeyPoint], list[np.ndarray]]:
     print(f"len before: {len(descriptors)}")
-    # initialize list to store idx values of records to drop
     to_drop = []
     for i, img_descriptors in enumerate(descriptors):
-        # if there are no descriptors, add record idx to drop list
         if img_descriptors is None:
             to_drop.append(i)
 
     print(f"indexes: {to_drop}")
-    # delete from list in reverse order
     for i in sorted(to_drop, reverse=True):
         del descriptors[i], keypoints[i]
 
@@ -44,20 +41,18 @@ def sift_features(images, clean=True) -> tuple[list[cv2.KeyPoint], list[np.ndarr
     keypoints_agg: list[cv2.KeyPoint] = []
     descriptors_agg: list[np.ndarray] = []
     for img in images:
-        # extract keypoints and descriptors for each image
         img_keypoints, img_descriptors = sift.detectAndCompute(img, None)
         keypoints_agg.append(img_keypoints)
         descriptors_agg.append(img_descriptors)
 
-    return clean_descriptors(
-        keypoints_agg, descriptors_agg
+    return (
+        clean_descriptors(keypoints_agg, descriptors_agg)
     ) if clean else keypoints_agg, descriptors_agg
 
 
 def get_visual_words(descriptors, codebook):
     visual_words = []
     for img_descriptors in descriptors:
-        # for each image, map each descriptor to the nearest codebook entry
         img_visual_words, distance = vq(img_descriptors, codebook)
         visual_words.append(img_visual_words)
     return visual_words
@@ -66,7 +61,6 @@ def get_visual_words(descriptors, codebook):
 def get_frequency_vectors(visual_words, k):
     frequency_vectors = []
     for img_visual_words in visual_words:
-        # create a frequency vector for each image
         img_frequency_vector = np.zeros(k)
         for word in img_visual_words:
             img_frequency_vector[word] += 1
@@ -105,7 +99,6 @@ def search_test(
     :return: indices of k the most similar images and their cosine similarity
     """
     if search_image is not None:
-        # assert search image is black and white
         assert len(search_image.shape) == 2
         print("Search image:")
         plt.imshow(search_image, cmap="gray")
@@ -115,15 +108,11 @@ def search_test(
     a = image
     b = db
 
-    # get the cosine distance for the search image `a`
     cosine_similarity = np.dot(a, b.T) / (norm(a) * norm(b, axis=1))
-    # get the top k indices for most similar vecs
     idx = np.argsort(-cosine_similarity)[:top_k]
 
-    # display the results
     if db_images:
         for i in idx:
-            # assert db images are black and white
             assert len(db_images[i].shape) == 2
             print(f"{i}: {round(cosine_similarity[i], 4)}")
             plt.imshow(db_images[i], cmap="gray")
